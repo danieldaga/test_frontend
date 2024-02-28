267d83c77c81eab4322b3637f124e3fa8e3fac3d
danieldaga <daniel@arhrestauracion.com>
2024-02-28 20:28:49 +0100

    cleancode

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,39 +1,38 @@
-import { Avatar, AvatarImage } from "./ui/avatar"
-import { 
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle
- } from "./ui/card"
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
- interface UserCardProps {
-    avatarUrl: string;
-    userName: string;
-    userMail: string;
-    userPhone: string ;
-  }
+interface UserCardProps {
+  avatarUrl: string;
+  userName: string;
+  userMail: string;
+  userPhone: string;
+}
 
-const UserCard: React.FC<UserCardProps> = ({ avatarUrl, userName, userMail, userPhone }) => {
-    return (
-        <Card>
-            <CardHeader>
-            <div className="flex justify-center">
-                <div className="w-24 h-24 flex justify-center items-center bg-gray-200 rounded-full">
-                    <Avatar className="">
-                        <AvatarImage src={avatarUrl} />
-                    </Avatar>
-                </div>
-            </div>
-                
-                <CardTitle>{userName}</CardTitle>
-            </CardHeader>
-            <CardContent>
-            <p>Email: {userMail}</p>
-            <p>Phone: {userPhone}</p>
-            </CardContent>
-        </Card>
-      
-    )
-  }
-  
-  export default UserCard
+const UserCard: React.FC<UserCardProps> = ({
+  avatarUrl,
+  userName,
+  userMail,
+  userPhone,
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex justify-center">
+          <div className="w-24 h-24 flex justify-center items-center bg-gray-200 rounded-full">
+            <Avatar className="">
+              <AvatarImage src={avatarUrl} />
+            </Avatar>
+          </div>
+        </div>
+
+        <CardTitle>{userName}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Email: {userMail}</p>
+        <p>Phone: {userPhone}</p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default UserCard;
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import UserCard from './UserCard';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import UserCard from "./UserCard";
 
 interface User {
   id: string;
@@ -16,10 +16,12 @@ const UsersList: React.FC = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get('https://random-data-api.com/api/v2/users?size=100');
+        const response = await axios.get(
+          "https://random-data-api.com/api/v2/users?size=100"
+        );
         setUsers(response.data);
       } catch (error) {
-        console.error('Error fetching users:', error);
+        console.error("Error fetching users:", error);
       }
     };
     fetchUsers();
@@ -27,15 +29,17 @@ const UsersList: React.FC = () => {
 
   return (
     <div>
-      {users.map(user => (
-          <div key={user.id} style={{ width: '30%', margin: '1%', float: 'left' }}>
-            <UserCard avatarUrl={`https://robohash.org/${user.id}`} userName={`${user.first_name} ${user.last_name}`} userMail={user.email} userPhone={user.phone_number} />
-          {/* <img src={`https://robohash.org/${user.id}`} alt="avatar" />
-          <div>
-            <p>Name: {`${user.first_name} ${user.last_name}`}</p>
-            <p>Email: {user.email}</p>
-            <p>Phone: {user.phone_number}</p>
-          </div> */}
+      {users.map((user) => (
+        <div
+          key={user.id}
+          style={{ width: "30%", margin: "1%", float: "left" }}
+        >
+          <UserCard
+            avatarUrl={`https://robohash.org/${user.id}`}
+            userName={`${user.first_name} ${user.last_name}`}
+            userMail={user.email}
+            userPhone={user.phone_number}
+          />
         </div>
       ))}
     </div>
