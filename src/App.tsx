33376a8833e58004494a12d8ad8bbbc8d55d0922
danieldaga <daniel@arhrestauracion.com>
2024-02-28 19:46:04 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+import { UsersTable } from './components/UsersTable'
+
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      {/* <UsersTable /> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <header className="bg-purple-900 py-6 br-20">
+      <div className="text-center py-4">
+        <h1 className="text-4xl font-bold text-gray-800">header</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
